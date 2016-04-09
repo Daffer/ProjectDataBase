00354621cd86a1998e7a6cd95863d743f1636b15
Daffer <catkiller95@gmail.com>
2016-04-09 20:52:59 +0300

    Add class BaseController and Papticipant
    Добавил классы для взаимодействия с базой и класс участника приложения

---
+++ ProjectDataBase/Participant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ProjectDataBase
+{
+    class Participant
+    {
+        private string Name;
+        private int ID;
+        public string NameOperation
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+        public int IDOperation
+        {
+            get { return ID; }
+            set { ID = value; }
+        }
+    }
+}
